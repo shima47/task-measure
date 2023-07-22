7a94e14c0b77926768d7af66c230fca2788a6f2a
shima47 <shimamura47t@gmail.com>
2023-07-22 20:50:42 +0900

    feat: データ変換関数

--- popup/common.ts
+++ popup/common.ts
@@ -19,6 +19,35 @@ export const formatElapsedTime = (milliseconds: number) => {
   return `${formattedHours}h ${formattedMinutes}m`;
 }
 
+const getDayTask = (orderData, index) => {
+  const taskStartIndex = orderData.indexOf(index) + 1;
+  const taskEndIndex = orderData.indexOf(index + 1);
+  
+  if (taskStartIndex === 0 || taskEndIndex === -1) {
+    return [];
+  }
+  
+  return orderData.slice(taskStartIndex, taskEndIndex);
+}
+
+const changeOrder = (orderData: (string | number)[], str: string, direction: "forward" | "backward" = "forward") => {
+  const currentIndex = orderData.indexOf(str);
+  if (currentIndex === -1) {
+    return orderData; // 該当する文字列が見つからない場合は元の配列を返す
+  }
+  
+  const nextIndex = direction === "forward" ? currentIndex + 1 : currentIndex - 1;
+  
+  if (nextIndex === 0 || nextIndex === orderData.length - 1) {
+    return orderData; // 端の要素と入れ替えようとする場合は元の配列を返す
+  }
+  
+  const copyOrderData = [...orderData]; // 元の配列を変更せずにコピー
+  // 配列の要素を入れ替え
+  [copyOrderData[currentIndex], copyOrderData[nextIndex]] = [copyOrderData[nextIndex], copyOrderData[currentIndex]];
+  
+  return copyOrderData;
+}
 
 export const updateTask = (allTaskState, newTask) => {
   const [allTask, setAllTask] = allTaskState
@@ -32,3 +61,4 @@ export const updateTask = (allTaskState, newTask) => {
   console.dir(newAllTask)
   setAllTask(newAllTask)
 }
+

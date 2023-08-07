b97cc352d10184e86a6710f8a510f2ee1338b478
shima47 <shimamura47t@gmail.com>
2023-08-08 07:54:18 +0900

    feat: totalDayTime

--- popup/DayDiv.tsx
+++ popup/DayDiv.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { v4 as uuid } from "uuid"
-import { createNewTask } from "./common"
+import { createNewTask, getTotalDayTime } from "./common"
 import Task from "./Task"
 import addIcon from "data-base64:~assets/add.svg"
 import accordionIcon from "data-base64:~assets/accordion.svg"
@@ -12,6 +12,9 @@ function dayDiv({ dayIndex, dOfW, dayTaskOrder, ...props }) {
   const [isOpenAry, setIsOpenAry] = props.grobalState.isOpenAryState
   const isOpen = isOpenAry[dayIndex]
 
+  // その曜日の合計時間
+  const totalTime = getTotalDayTime(dayTaskOrder, props.grobalState.allTaskState)
+
   const onClickDayTitle = () => {
     const newIsOpenAry = [...isOpenAry]
     newIsOpenAry[dayIndex] = !isOpenAry[dayIndex]
@@ -21,7 +24,7 @@ function dayDiv({ dayIndex, dOfW, dayTaskOrder, ...props }) {
   const onClickNewTask = (event) => {
     // 親コンポーネントへのイベントの伝搬を防ぐ
     event.stopPropagation()
-    
+
     // 新規作成時はアコーディオンを開く
     const newIsOpenAry = [...isOpenAry]
     newIsOpenAry[dayIndex] = true
@@ -33,10 +36,9 @@ function dayDiv({ dayIndex, dOfW, dayTaskOrder, ...props }) {
   return (
     <div className="dayDiv">
       <div className="dayTitleDiv" onClick={onClickDayTitle}>
-        <div className="dayTitile">
-          <img className={isOpen ? "accordionOpen" : "accordionClose"} src={accordionIcon} alt="開く" />
-          {dOfW}
-        </div>
+        <img className={isOpen ? "accordionOpen" : "accordionClose"} src={accordionIcon} alt="開く" />
+        <div className="dayTitile">{dOfW}</div>
+        <div className="totalTime">{totalTime.toFixed(2)} h</div>
         <div className="btn" onClick={onClickNewTask}>
           <img src={addIcon} alt="新規追加" />
         </div>
--- popup/common.ts
+++ popup/common.ts
@@ -26,6 +26,19 @@ export const getDayTaskOrder = (orderData, index) => {
   return dayTaskOrder;
 }
 
+/**
+ * タスク時間の合計を返す関数
+ * @param dayTaskOrder その曜日のタスクIDの並び順
+ * @param allTaskState 全てのタスクデータ
+ * @returns 0埋めされた合計
+ */
+export const getTotalDayTime = (dayTaskOrder: string[], allTaskState) => {
+  const [allTask, setAllTask] = allTaskState
+  const dayTimeAry: number[] = dayTaskOrder.map(taskId => allTask[taskId]["time"])
+  // 配列の時間を全て合計する
+  const totalTime: number = dayTimeAry.reduce((sum, time) => sum + time, 0)
+  return totalTime
+};
 
 export const getChangedOrder = (orderData: (string | number)[], taskId: string, direction: "forward" | "backward" = "forward") => {
   const currentIndex = orderData.indexOf(taskId);

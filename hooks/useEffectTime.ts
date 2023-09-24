7f42a443dada1dc4627a4f7914a23d6bb92872d4
shima47 <shimamura47t@gmail.com>
2023-09-24 22:55:30 +0900

    refactor: useTaskTimeのuseElapsedTIme対応

--- hooks/useEffectTime.ts
+++ hooks/useEffectTime.ts
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
-import useRunningTaskInfo from "./useRunningTaskInfo";
+import { useEffect, } from "react";
+import useUpdateTask from "./useUpdateTask";
 import useIsRunning from "./useIsRunning";
-import useApplyTimeDiff from "./useApplyTimeDiff";
+import useRunningTaskInfo from "./useRunningTaskInfo";
+import useElapsedTime from "./useElapsedTime";
 
 
 const useEffectTime = (taskId: string) => {
   const isRunning = useIsRunning(taskId)
+  const { getElapsedTime } = useElapsedTime(taskId)
+  const { updateTaskTime } = useUpdateTask()
   const [, { setStartTimeNow }] = useRunningTaskInfo()
 
+  // 実行中のタスクは実行時間を更新する
   useEffect(() => {
     // 実行中のタスクじゃないなら終了
     if (!isRunning) return
     // 未反映の経過時間分を加算する
-    useApplyTimeDiff(taskId)
+    const elapsedTime = getElapsedTime()
+    updateTaskTime(taskId, elapsedTime)
     // 開始時間をリセットする
     setStartTimeNow()
   }, [])

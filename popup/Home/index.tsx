3800bcc7978dd076b60cd74b54c973f5365c7d98
shima47 <shimamura47t@gmail.com>
2023-09-25 20:09:50 +0900

    refactor: globalStateの消去

--- popup/Home/index.tsx
+++ popup/Home/index.tsx
@@ -1,30 +1,9 @@
-import { useContext } from "react"
-import * as context from "~components/Provider/MyProvider";
 import DayDiv from "~components/DayDiv"
 
 
 const DAY_OF_WEEK_ARY = ["Mon.", "Tue.", "Wed.", "Thu.", "Fri.",]
 
 const Home = () => {
-  const [allTask, setAllTask] = useContext(context.allTaskContext)
-  const [order, setOrder] = useContext(context.orderContext)
-
-  const [runningTask, setRunningTask] = useContext(context.runningTaskInfoContext)
-  const [isOpenAry, setIsOpenAry] = useContext(context.isOpenAryContext)
-
-  const [selectedTaskId, setSelectedTaskId] = useContext(context.selectedTaskIdContext)
-
-  console.dir(allTask)
-
-
-  const grobalState = {
-    allTaskState: [allTask, setAllTask],
-    orderDataState: [order, setOrder],
-    isOpenAryState: [isOpenAry, setIsOpenAry],
-    runningTask: [runningTask, setRunningTask],
-    selectedTaskIdState: [selectedTaskId, setSelectedTaskId],
-  }
-
   return (
     <div className="home">
       {
@@ -33,7 +12,6 @@ const Home = () => {
             key={item}
             dayIndex={index}
             dOfW={item}
-            grobalState={grobalState}
           />
         })
       }
--- popup/Page.tsx
+++ popup/Page.tsx
@@ -1,34 +1,17 @@
-import { useContext } from "react"
-import * as context from "~components/Provider/MyProvider";
 import Header from "~components/Header";
-import DataImport from "./importData"
 import Home from "./Home";
+import DataImport from "./importData"
 import useIsImporting from "~features/importData/useIsImporting";
 
 
 const Page = () => {
-  const [allTask, setAllTask] = useContext(context.allTaskContext)
-  const [order, setOrder] = useContext(context.orderContext)
-
-  const [runningTask, setRunningTask] = useContext(context.runningTaskInfoContext)
-  const [isOpenAry, setIsOpenAry] = useContext(context.isOpenAryContext)
-
-  const [selectedTaskId, setSelectedTaskId] = useContext(context.selectedTaskIdContext)
   // JSONのインポート画面を切り替える
   const [isImporting,] = useIsImporting()
 
 
-  const grobalState = {
-    allTaskState: [allTask, setAllTask],
-    orderDataState: [order, setOrder],
-    isOpenAryState: [isOpenAry, setIsOpenAry],
-    runningTask: [runningTask, setRunningTask],
-    selectedTaskIdState: [selectedTaskId, setSelectedTaskId],
-  }
-
   return (
     <div className="page">
-      <Header grobalState={grobalState} />
+      <Header />
       <div className="container">
         {
           isImporting ?

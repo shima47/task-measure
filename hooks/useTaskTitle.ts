d84da28d6296bdc947649a66a9364429848f33b4
shima47 <shimamura47t@gmail.com>
2023-09-27 19:14:38 +0900

    fix: 古いタイトルが保存されていた

--- hooks/useTaskTitle.ts
+++ hooks/useTaskTitle.ts
@@ -4,13 +4,13 @@ import useUpdateTask from "~hooks/useUpdateTask";
 
 
 const useTaskTitle = (taskId: string) => {
-  const [allTask, setAllTask] = useContext(context.allTaskContext)
+  const [allTask,] = useContext(context.allTaskContext)
   const [taskTitle, setTaskTitle] = useState(allTask[taskId]["title"])
   const { updateTaskTitle } = useUpdateTask()
 
   const onChangeTitle = (event) => {
+    updateTaskTitle(taskId, event.target.value)
     setTaskTitle(event.target.value)
-    updateTaskTitle(taskId, taskTitle)
   }
 
   return [taskTitle, onChangeTitle] as const

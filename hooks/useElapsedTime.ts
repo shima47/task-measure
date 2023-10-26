dc15a918e3b671dae390542e40d8e5b559262ca7
shima47 <shimamura47t@gmail.com>
2023-10-26 19:51:38 +0900

    feat: 複数の休憩時間に対応

--- hooks/useElapsedTime.ts
+++ hooks/useElapsedTime.ts
@@ -1,6 +1,8 @@
+import * as type from "~types/type"
 import useTask from "./useTask";
 import useRunningTaskInfo from "./useRunningTaskInfo";
-import { restTimeSetting } from "~features/restTime/consts";
+import { getTotalRestTime } from "~features/restTime/getTotalRestTime";
+
 
 /**
  * 経過時間を加えたタスク時間を返すカスタムフック
@@ -10,37 +12,21 @@ const useElapsedTime = (taskId: string) => {
   const task = useTask(taskId)
   const [runningTaskInfo,] = useRunningTaskInfo()
 
-  const getElapsedTime = () => {
+  // 未反映のタスク実行時間を返す
+  const getElapsedTime = async () => {
     if (!task) return
+    // 実行開始時間と現在時間
+    const taskStartTime = runningTaskInfo.startTime
+    const now = Date.now()
 
-    // 休憩開始時間
-    const restStartDate = new Date()
-    restStartDate.setHours(restTimeSetting.startHour)
-    restStartDate.setMinutes(restTimeSetting.startMin)
-    const restStartTime = restStartDate.getTime()
-    // 休憩終了時間
-    const restEndDate = new Date()
-    restEndDate.setHours(restTimeSetting.endHour)
-    restEndDate.setMinutes(restTimeSetting.endMin)
-    const restEndTime = restEndDate.getTime()
+    // 経過時間と休憩時間のオーバーラップ合計を取得する
+    const totalRestTime = await getTotalRestTime(taskStartTime, now)
 
-    const startTime = runningTaskInfo.startTime
-    const endTime = Date.now()
-
-    // 実行時間が休憩時間と重複しないとき
-    if (endTime < restStartTime || restEndTime < startTime) {
-      const timeDifference = endTime - startTime
-      return ((parseFloat(task.time) * 3600000) + timeDifference) / 3600000
-    }
-    // 実行時間が休憩時間と重複するとき
-    else {
-      // 開始/停止時間が休憩時間内のときその部分は加算しないので0にする
-      const timeDifference1 = restStartTime - startTime < 0 ? 0 : restStartTime - startTime
-      const timeDifference2 = endTime - restEndTime < 0 ? 0 : endTime - restEndTime
-      const totalDifference = timeDifference1 + timeDifference2
-      return ((parseFloat(task.time) * 3600000) + totalDifference) / 3600000
-    }
+    // 実行時間から休憩時間を引く（0以下なら0にする）
+    const elapsedTime = (now - taskStartTime) - totalRestTime > 0 ? (now - taskStartTime) - totalRestTime : 0
+    return ((parseFloat(task.time) * 3600000) + elapsedTime) / 3600000
   }
+
 
   return { getElapsedTime } as const
 }

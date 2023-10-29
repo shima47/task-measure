eb58abc5c5ed2f741caa7bc5ee361f87858261c9
shima47 <shimamura47t@gmail.com>
2023-10-29 23:33:58 +0900

    style: 休憩時間追加ボタン配置

--- popup/Setting/index.tsx
+++ popup/Setting/index.tsx
@@ -1,15 +1,27 @@
+import { Tooltip } from "react-tooltip";
 import RestTime from "~components/RestTime"
+import addIcon from "data-base64:~assets/add.svg"
 
 
 const Setting = () => {
   return (
     <div className="setting">
       <div className="settingItem">
-        <div className="settingTitle">Rest Time</div>
-        <div className="settingInput">
+        <div className="settingLeft">
+          <div className="settingTitle">Rest Time</div>
+          <div className="btn" id="addBtn" onClick={() => { }}>
+            <img src={addIcon} alt="追加" />
+            <Tooltip anchorSelect="#addBtn" content='追加' place='bottom' delayShow={700} />
+          </div>
+        </div>
+        <div className="settingRight">
           <RestTime></RestTime>
         </div>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   )
 }

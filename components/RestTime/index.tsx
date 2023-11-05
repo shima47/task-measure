9f4ebdbc3b821c990625517b0be6ec5978016acd
shima47 <shimamura47t@gmail.com>
2023-11-05 18:24:25 +0900

    feat: 削除ボタン配置

--- components/RestTime/index.tsx
+++ components/RestTime/index.tsx
@@ -1,4 +1,6 @@
+import { Tooltip } from 'react-tooltip'
 import useRestTime from "~features/restTime/useRestTime"
+import deleteIcon from "data-base64:~assets/delete.svg"
 
 const RestTime = ({ restTimeIndex }) => {
   const [restTime, { onChangeStartRestTime, onChangeEndRestTime, onChangeSelect }] = useRestTime(restTimeIndex)
@@ -10,6 +12,10 @@ const RestTime = ({ restTimeIndex }) => {
       <input className="timeTypeInput" type="time" value={restTime.start} onChange={onChangeStartRestTime} />
       <div>～</div>
       <input className="timeTypeInput" type="time" value={restTime.end} onChange={onChangeEndRestTime} />
+      <div className="restTimeDeleteBtn" id="deleteBtn" onClick={()=>{}}>
+          <img src={deleteIcon} alt="削除"></img>
+          <Tooltip anchorSelect="#deleteBtn" content='削除' place='bottom' delayShow={700} />
+        </div>
     </div>
   )
 }

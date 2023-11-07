7ee596352d1e116334b0a6887bda8f0b19e2bc7b
shima47 <shimamura47t@gmail.com>
2023-11-07 23:28:29 +0900

    Revert "feat: transferBtn設置"
    This reverts commit 433cc0826f593e6a5816db2c9d18690350bc8272.

--- components/Task/index.tsx
+++ components/Task/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { Tooltip } from "react-tooltip"
 import useRunTask from "~features/runTask/useRunTask"
 import useTaskTitle from "~hooks/useTaskTitle"
@@ -9,8 +8,6 @@ import useTaskTime from "~hooks/useTaskTime"
 import useEffectTime from "~hooks/useEffectTime"
 import startIcon from "data-base64:~assets/start.svg"
 import stopIcon from "data-base64:~assets/stop.svg"
-import linkIcon from "data-base64:~assets/link.svg"
-import activeLinkIcon from "data-base64:~assets/activeLink.svg"
 
 
 /**
@@ -32,8 +29,6 @@ const Task = ({ taskId }) => {
   // 経過時間を反映させる
   useEffectTime(taskId)
 
-  const [isTransfer, setIsTranfer] = useState(false)
-
   return (
     <div className="task" >
       <input
@@ -42,10 +37,6 @@ const Task = ({ taskId }) => {
         checked={isSelected}
         onChange={onChangeSelect}
       />
-      <div className={`transferBtn ${isTransfer && "activeTransferBtn"}`} id="transferBtn" onClick={()=>{setIsTranfer(c => !c)}}>
-        <img src={isTransfer ? activeLinkIcon : linkIcon} alt="移行先"></img>
-        <Tooltip anchorSelect="#transferBtn" content='移行先' place='bottom' delayShow={700} />
-      </div>
       <div className="taskTitle">
         <input
           className="taskForm"
